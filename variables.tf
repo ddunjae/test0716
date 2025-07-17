692e7b8c4b5ee5be859a155388d26b4e47e0c8ff
ddunjae <hsj960919@gmail.com>
2025-07-17 15:23:15 +0900

    fix:webapp &plan

--- variables.tf
+++ variables.tf
@@ -130,21 +130,19 @@ variable "app_service_name" {
   default = "tes0pwebapp0717"
 }
 
-variable "storage_account_name" {
+//Storage Account
+variable "stoage_account_name" {
   type    = string
-  default = "testconorstorage0717"
+  default = "testconorstorage0717"  
 }
-
-variable "storage_account_tier" {
-  type    = string
+variable "stoage_account_tier" {
+  type = string
   default = "Standard"
 }
-
-variable "storage_account_replication_type" {
-  type    = string
+variable "stoage_account_replication_type" {
+  type = string
   default = "LRS"
 }
-
 variable "container_name" {
   type    = string
   default = "tfstate"
